389e6e95f906dd88adcff9af9a45239504aaabb7
Sneha8934 <snehamane004@gmail.com>
2020-10-11 13:07:42 +0530

    ADDED:While Loop

---
+++ WhileLoop/PowerOfNumber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+read -p "Enter the number: " number
+power=1
+
+while [ $number -ne 0 ]
+do
+	power=$(( power * 2 ))
+	if [ $power -le 256 ]
+	then
+		echo $power
+	fi
+	((--number))
+done
